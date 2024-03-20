bf43795971c477931a8ff7e1f792a6abd35f5a06
Abhishek Nalatawad <134145130+Abhishek-PJ@users.noreply.github.com>
2024-03-20 06:51:00 +0530

    Update Knapsack_Greedy.c

--- Knapsack_Greedy.c
+++ Knapsack_Greedy.c
@@ -1,51 +1,41 @@
 #include <stdio.h>
 
-// Function to find the maximum of two integers
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
+// Structure to represent each item
+struct Item {
+    int value;
+    int weight;
+};
 
-// Function to solve 0/1 Knapsack problem using Dynamic Programming
-int knapSack(int knapsackCapacity, int itemWeights[], int itemValues[], int numOfItems) {
-    int i, j;
-    int maxValues[numOfItems + 1][knapsackCapacity + 1]; // Create a 2D array for storing the maximum value at each combination of items and weights
-
-    // Build maxValues[][] in bottom-up manner
-    for (i = 0; i <= numOfItems; i++) {
-        for (j = 0; j <= knapsackCapacity; j++) {
-            // Base case: if no items or capacity is 0, value is 0
-            if (i == 0 || j == 0)
-                maxValues[i][j] = 0;
-            else if (itemWeights[i - 1] <= j) {
-                // If the weight of the current item is less than or equal to the current capacity
-                // Choose the maximum between including the current item or excluding it
-                // itemValues[i - 1] represents the value of the current item
-                // itemWeights[i - 1] represents the weight of the current item
-                // maxValues[i - 1][j - itemWeights[i - 1]] represents the maximum value without including the current item's weight
-                maxValues[i][j] = max(itemValues[i - 1] + maxValues[i - 1][j - itemWeights[i - 1]], maxValues[i - 1][j]);
-            } else {
-                // If the weight of the current item is more than the current capacity, exclude it
-                maxValues[i][j] = maxValues[i - 1][j];
-            }
+// Function to solve Knapsack problem using Greedy approach
+double knapsack(int capacity, struct Item items[], int n) {
+    double total_value = 0.0; // Total value of items taken
+    
+    // Fill the knapsack with items in decreasing order of value-to-weight ratio
+    for (int i = 0; i < n; i++) {
+        if (capacity >= items[i].weight) {
+            total_value += items[i].value;
+            capacity -= items[i].weight;
+        } else {
+            // Take fraction of the item
+            total_value += ((double)capacity / items[i].weight) * items[i].value;
+            break;
         }
     }
 
-    // Return the maximum value that can be obtained with the given capacity and items
-    return maxValues[numOfItems][knapsackCapacity];
+    return total_value;
 }
 
-// Main function to test above function
 int main() {
-    int itemValues[] = {60, 100, 120}; // Values of the items
-    int itemWeights[] = {10, 20, 30}; // Weights of the items
-    int knapsackCapacity = 50; // Capacity of the knapsack
-    int numOfItems = sizeof(itemValues) / sizeof(itemValues[0]); // Number of items
-    int maxVal = knapSack(knapsackCapacity, itemWeights, itemValues, numOfItems); // Find the maximum value
-    printf("Items available for knapsack problem:\n");
-    for (int i = 0; i < numOfItems; i++) {
-        printf("Item %d: Weight = %d, Value = %d\n", i + 1, itemWeights[i], itemValues[i]); // Print the details of each item
-    }
-    printf("Capacity of the knapsack: %d\n", knapsackCapacity); // Print the capacity of the knapsack
-    printf("Maximum value that can be obtained: %d\n", maxVal); // Print the maximum value that can be obtained
+    // Sample items and capacity
+    struct Item items[] = {{60, 10}, {100, 20}, {120, 30}};
+    int capacity = 50;
+    int n = sizeof(items) / sizeof(items[0]);
+
+    // Calculate the maximum value using the Greedy approach
+    double max_value = knapsack(capacity, items, n);
+
+    // Print the maximum value
+    printf("Maximum value: %.2f\n", max_value);
+
     return 0;
 }
